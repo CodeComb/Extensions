71577d8998e17c2d20d1429a470a0a0259ee6078
あまみや ゆうこ <1@1234.sh>
2015-11-03 10:50:35 +0800

    React to Microsoft.Extensions.WebEncoder

--- src/CodeComb.AspNet.Extensions/Others/HtmlContentExtensions.cs
+++ src/CodeComb.AspNet.Extensions/Others/HtmlContentExtensions.cs
@@ -1,23 +1,94 @@
-﻿using System.IO;
-using Microsoft.Extensions.WebEncoders;
+﻿using System;
+using System.IO;
+using System.Text.Encodings.Web;
 
 namespace Microsoft.AspNet.Mvc.Rendering
 {
-    internal class CodeCombHtmlEncoder : IHtmlEncoder
+    internal class CodeCombHtmlEncoder : HtmlEncoder
     {
-        public string HtmlEncode(string value)
+        public override int MaxOutputCharactersPerInputCharacter
         {
-            return value;
+            get { return 1; }
         }
 
-        public void HtmlEncode(string value, int startIndex, int charCount, TextWriter output)
+        public override string Encode(string value)
         {
-            output.Write(value.Substring(startIndex, charCount));
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (value.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            return $"HtmlEncode[[{value}]]";
         }
 
-        public void HtmlEncode(char[] value, int startIndex, int charCount, TextWriter output)
+        public override void Encode(TextWriter output, char[] value, int startIndex, int characterCount)
         {
-            output.Write(value, startIndex, charCount);
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (characterCount == 0)
+            {
+                return;
+            }
+            
+            output.Write(value, startIndex, characterCount);
+        }
+
+        public override void Encode(TextWriter output, string value, int startIndex, int characterCount)
+        {
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (characterCount == 0)
+            {
+                return;
+            }
+            
+            output.Write(value.Substring(startIndex, characterCount));
+        }
+
+        public override bool WillEncode(int unicodeScalar)
+        {
+            return false;
+        }
+
+        public override unsafe int FindFirstCharacterToEncode(char* text, int textLength)
+        {
+            return -1;
+        }
+
+        public override unsafe bool TryEncodeUnicodeScalar(
+            int unicodeScalar,
+            char* buffer,
+            int bufferLength,
+            out int numberOfCharactersWritten)
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            numberOfCharactersWritten = 0;
+            return false;
         }
     }
     public static class HtmlContentExtensions
